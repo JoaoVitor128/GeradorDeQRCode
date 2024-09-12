7d3665505e22c02f3c42f22a233b8198ae34f67a
João Vitor <157144910+JoaoVitor128@users.noreply.github.com>
2024-09-12 15:03:51 -0300

    Add files via upload

---
+++ src/js/script.js
@@ -0,0 +1,39 @@
+const container = document.querySelector(".container");
+const qrCodeBtn = document.querySelector(".qrForm button");
+
+const qrCodeInput = container.querySelector(".qrForm input");
+const qrCodeImg = container.querySelector(".QRCode img");
+
+// Gerar código
+function generateQrCode() {
+  let qrCodeInputValue = qrCodeInput.value;
+
+  if (!qrCodeInputValue) return;
+
+  qrCodeBtn.innerText = "Gerando código...";
+
+  qrCodeImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrCodeInputValue}`;
+
+  qrCodeImg.addEventListener("load", () => {
+    container.classList.add("active");
+    qrCodeBtn.innerText = "Código criado!";
+  });
+}
+
+qrCodeBtn.addEventListener("click", () => {
+  generateQrCode();
+});
+
+qrCodeInput.addEventListener("keydown", (e) => {
+  if (e.code === "Enter") {
+    generateQrCode();
+  }
+});
+
+// Limpar área do código
+qrCodeInput.addEventListener("keyup", () => {
+  if (!qrCodeInput.value) {
+    container.classList.remove("active");
+    qrCodeBtn.innerText = "Gerar QR Code";
+  }
+});
